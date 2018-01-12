4aae0fb260d981d3b91d618a19d556884a5342f5
hrtanderson <hrtanderson@gmail.com>
2018-01-12 18:15:04 +0100

    modified archive

--- archive.php
+++ archive.php
@@ -31,27 +31,22 @@ get_header(); ?>
 				?>
 			</header><!-- .page-header -->
 
-			<?php
-			// Start the Loop.
-			while ( have_posts() ) : the_post();
+			<?php the_post(); ?>
+			<h1 class="entry-title"><?php the_title(); ?></h1>
+			
+			<?php get_search_form(); ?>
+			
+			<h2>Archives by Month:</h2>
+			<ul>
+				<?php wp_get_archives('type=monthly'); ?>
+			</ul>
+			
+			<h2>Archives by Subject:</h2>
+			<ul>
+					<?php wp_list_categories(); ?>
+			</ul>
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
-
-			// End the loop.
-			endwhile;
-
-			// Previous/next page navigation.
-			the_posts_pagination( array(
-				'prev_text'          => __( 'Previous page', 'twentysixteen' ),
-				'next_text'          => __( 'Next page', 'twentysixteen' ),
-				'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentysixteen' ) . ' </span>',
-			) );
-
+		<?php
 		// If no content, include the "No posts found" template.
 		else :
 			get_template_part( 'template-parts/content', 'none' );
