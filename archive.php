477f9b84c48b78c082c899c86b64c8f7a17f3259
hrtanderson <hrtanderson@gmail.com>
2018-01-12 18:16:47 +0100

    modified archive

--- archive.php
+++ archive.php
@@ -35,15 +35,20 @@ get_header(); ?>
 			<h1 class="entry-title"><?php the_title(); ?></h1>
 			
 			<?php get_search_form(); ?>
+
+			<h2>Archives by Tag:</h2>
+			<ul>
+					<?php wp_list_tags(); ?>
+			</ul>
+
+			<h2>Archives by Category:</h2>
+			<ul>
+					<?php wp_list_categories(); ?>
+			</ul>			
 			
 			<h2>Archives by Month:</h2>
 			<ul>
 				<?php wp_get_archives('type=monthly'); ?>
-			</ul>
-			
-			<h2>Archives by Subject:</h2>
-			<ul>
-					<?php wp_list_categories(); ?>
 			</ul>
 
 		<?php
