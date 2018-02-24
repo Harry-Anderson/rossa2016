eb23803227694c616f63ec5330f7d6ff2e5f67c9
hrtanderson <hrtanderson@gmail.com>
2018-02-24 15:26:00 +0100

    modified front page

--- front-page.php
+++ front-page.php
@@ -97,13 +97,11 @@ get_header(); ?>
 						echo '<header class="entry-header"><h2 class="entry-title">';
 							echo 'Latest Blog Post: ' . $recent['post_title'];
 						echo '</h2></header>';
+						
 						$content_post = get_post($recent["ID"]);
 						$content = $content_post->post_content;
 						
-						$excerpt = wp_trim_excerpt( $content); // $excerpt contains the excerpt of the concerned post
-
-						$excerpt = str_replace(']]>', ']]&gt;', $excerpt);
-						$excerpt = str_replace("\r", "<br />", $excerpt);
+						$excerpt = wp_trim_excerpt($content);
 						
 						echo $excerpt;
 					echo '</div>';
